7d2413ae2223b49663490dbfed11f263e3e9417d
Jalastin <57930712+Jalastin@users.noreply.github.com>
2020-09-14 17:07:27 -0700

    Add files via upload

--- Granblue/js/scripts.js
+++ Granblue/js/scripts.js
@@ -7,3 +7,9 @@ function hideDrop(element) {
     var dropId = element.alt;
     document.getElementById(dropId).className = "dropoff";
 }
+function hideThis(element){
+    element.style.display = "none";
+}
+function showThis(element){
+    element.style.display = "inline-block";
+}
